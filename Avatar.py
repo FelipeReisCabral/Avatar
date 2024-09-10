b0fce0db3b4f6499028a98bed3e8ef9e9ae1bc31
Felipe Cabral <felipereiscabral@gmail.com>
2024-09-10 20:13:21 -0300

    1º dia do 7daysofcode consumindo uma API de Avatar com Python

--- Avatar.py
+++ Avatar.py
@@ -1,6 +1,13 @@
 import requests
 
+# Define a URL da API que contém informações sobre personagens do Avatar: The Last Airbender
 link = 'https://last-airbender-api.fly.dev/api/v1/characters'
 
+# Faz uma requisição HTTP GET para a URL especificada
+# A função requests.get() envia uma solicitação para buscar os dados da API
 requisicao = requests.get(link)
+
+# Converte a resposta da API para o formato JSON e imprime no console
+# O método json() transforma o conteúdo da resposta, que geralmente é em formato texto,
+# para um objeto Python que representa um dicionário JSON.
 print(requisicao.json())
